a2e72353088a06e7c3f4d5d670325f2069e1125f
CaioslppUO <caioslppuo@gmail.com>
2022-06-20 10:59:00 -0300

    Removed old files

--- src/api/action-type/controllers/action-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  action-type controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::action-type.action-type');
