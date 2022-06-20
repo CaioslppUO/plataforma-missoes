684efa4fb27d90c71d4cd6c6e3095eb59068364d
CaioslppUO <caioslppuo@gmail.com>
2022-06-20 11:18:32 -0300

    Added projects files

---
+++ knexfile.ts
@@ -0,0 +1,52 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: __dirname + "/src/database/test.sqlite3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: __dirname + "/src/database/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/src/database/seeds",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
